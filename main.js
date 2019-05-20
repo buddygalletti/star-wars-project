3e48e6d1f4fa24058cdefc50b1639f7c936d2579
Buddy Galletti <buddygalletti@gmail.com>
2019-05-19 18:38:43 -0700

    Added defer. Thanks Blake.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("I am in");
-
 async function goFetch() {
     try {
         const response = await fetch('https://swapi.co/api/people/?format=json');
@@ -11,16 +9,33 @@ async function goFetch() {
 }
 
 async function renderNames() {
-    let people = await goFetch();
-    people = people.results;
-    namesList = document.getElementById('names');
-    const html = people.map(function(person){
-        const item = `<li>${person.name}</li>`;
-        return item;
-    }).join('');
-    namesList.innerHTML += html;
+    try {
+        let people = await goFetch();
+        people = people.results;
+        namesList = document.getElementById('names');
+        const html = people.map(function(person){
+            const item = `<li>${person.name}</li>`;
+            return item;
+        }).join('');
+        namesList.innerHTML += html;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+// const container = document.getElementsByClassName('flex-container');
+// console.log(container);
+// container.addEventListener('click', function(event){
+//     console.log(event.target);
+// });
+
+
+
+
+
+
+
+
+
+
 renderNames();
-
-
